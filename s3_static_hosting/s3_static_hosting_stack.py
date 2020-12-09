306a36f36b5e5108a28e6f69cb41134bd5ebac55
Guillermo Mansilla <mansillag@gmail.com>
2020-12-09 21:40:18 +0000

    initial commit

--- s3_static_hosting/s3_static_hosting_stack.py
+++ s3_static_hosting/s3_static_hosting_stack.py
@@ -1,9 +1,28 @@
 from aws_cdk import core
 
+from aws_cdk import (
+    aws_s3 as s3,
+    core as cdk,
+)
+
+from aws_cdk.aws_s3_deployment import BucketDeployment, Source
+import random
+
 
 class S3StaticHostingStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        website_bucket = s3.Bucket(self, 
+            "MyHostingBucketMF",
+            versioned=True,
+            public_read_access=True,
+            website_index_document="index.html",
+            removal_policy=cdk.RemovalPolicy.DESTROY
+        )
+
+        BucketDeployment(self, "DeployStaticWebsite",
+            sources=[Source.asset("./website-dist")],
+            destination_bucket=website_bucket
+        )
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="s3_static_hosting"),
 
     install_requires=[
-        "aws-cdk.core==1.76.0",
+        "aws-cdk.core==1.77.0",
     ],
 
     python_requires=">=3.6",
